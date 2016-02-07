107bb24326da293cbf9830a6d34c017172e90cfc
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-07 09:10:29 -0500

    Set up resources for todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'todos#index'
+
+  resources :todos 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
