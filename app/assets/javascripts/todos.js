72dfa4e8ff6a64c5f879c55b678af3f1b4ec374e
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-08 05:29:44 -0500

    Add JS to reload page when ajax:success

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,6 +1,6 @@
 $(document).on('ready page:load', function() {
   $(".best_in_place").best_in_place();
-  $(".my-form, #completed, .delete").on("ajax:success", function(e, data, status, xhr) {
+  $(".my-form, #completed, .delete, .all-complete, .clear").on("ajax:success", function(e, data, status, xhr) {
     location.reload();
   });
 });
