43b7c95bdb3936e4195d84c8570ad8de21ef000b
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-08 05:26:36 -0500

    Hook up route for clear complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :todos 
 
   get 'complete' => 'todos#all_complete'
+  get 'clear' => 'todos#clear_complete'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
