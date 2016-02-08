1aed9c63df3c1b317717ffbc0fc34ffa2808d1c4
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-08 05:15:58 -0500

    Hook up all complete route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :todos 
 
+  get 'complete' => 'todos#all_complete'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
