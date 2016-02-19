a8e786216d34f222a32327d37a859237f9bcc9c2
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-19 19:25:18 +0000

    Remove unneeded functions

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,6 +1,8 @@
 $(document).on('ready page:load', function() {
   $(".best_in_place").best_in_place();
-  $(".my-form, #completed, .delete, .all-complete, .clear").on("ajax:success", function(e, data, status, xhr) {
-    location.reload();
+
+  $(".my-form").bind("ajax:success", function(e, data, status, xhr) {
+    var $this = $(this);
+    $this.find('input:text,textarea').val('').focus();
   });
 });
